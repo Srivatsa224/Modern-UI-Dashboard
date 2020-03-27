91c9496318bb91cf0a73086d4c7d509b44510071
srivatsa224 <srivatsa224@gmail.com>
2020-03-27 20:03:02 +0530

    Initial commit

---
+++ app/src/main/java/com/srivatsa/modern_ui_dashboard/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.srivatsa.modern_ui_dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
